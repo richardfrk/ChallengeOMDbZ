0a5c12259b982ccec430b80613c1bcca6b804131
Richard Frank <richardfrk@gmail.com>
2017-02-20 23:45:41 -0300

    Schema Model Changed.

--- ChallengeOMDbZ/MovieEntity+CoreDataProperties.swift
+++ ChallengeOMDbZ/MovieEntity+CoreDataProperties.swift
@@ -1,6 +1,6 @@
 //
 //  MovieEntity+CoreDataProperties.swift
-//  DesafioZUP
+//  ChallengeOMDbZ
 //
 //  Created by Richard Frank on 20/02/17.
 //  Copyright © 2017 Richard Frank. All rights reserved.
@@ -16,23 +16,14 @@ extension MovieEntity {
         return NSFetchRequest<MovieEntity>(entityName: "MovieEntity");
     }
 
-    @NSManaged public var mmActors: String?
-    @NSManaged public var mmAwards: String?
-    @NSManaged public var mmDirector: String?
     @NSManaged public var mmGenre: String?
     @NSManaged public var mmIMDbID: String?
-    @NSManaged public var mmIMDbRating: String?
-    @NSManaged public var mmIMDbVotes: String?
-    @NSManaged public var mmLanguage: String?
-    @NSManaged public var mmMetascore: String?
     @NSManaged public var mmPlot: String?
     @NSManaged public var mmPoster: String?
     @NSManaged public var mmRated: String?
     @NSManaged public var mmReleased: String?
     @NSManaged public var mmRuntime: String?
     @NSManaged public var mmTitle: String?
-    @NSManaged public var mmType: String?
-    @NSManaged public var mmWriter: String?
     @NSManaged public var mmYear: String?
 
 }
